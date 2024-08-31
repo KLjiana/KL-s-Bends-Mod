5f7ee3dceeca25cdfa1de77cd43808f2d3a8da45
KL_jiana <2436262516@qq.com>
2024-08-31 19:39:09 +0800

    蹲下动画

--- src/main/java/com/kljiana/kl_bends/util/PlayerUtil.java
+++ src/main/java/com/kljiana/kl_bends/util/PlayerUtil.java
@@ -4,14 +4,14 @@ import net.minecraft.client.player.AbstractClientPlayer;
 
 public class PlayerUtil {
     public static boolean isPlayerWalk(AbstractClientPlayer player){
-        return player.getDeltaMovement().horizontalDistanceSqr() > 0.01;
+        return player.getDeltaMovement().horizontalDistanceSqr() > 0D;
     }
 
     public static boolean isPlayerRun(AbstractClientPlayer player){
-        return player.getDeltaMovement().horizontalDistanceSqr() > 0.02;
+        return player.getDeltaMovement().horizontalDistanceSqr() > 0.02D;
     }
 
     public static boolean isPlayerJump(AbstractClientPlayer player){
-        return !player.onGround() && player.getDeltaMovement().y > 0;
+        return !player.onGround() && player.getDeltaMovement().y > 0D;
     }
 }
